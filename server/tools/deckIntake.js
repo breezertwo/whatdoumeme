492a18d31e6c5ebda3cff5aaf6dec995e4da0c15
Andy <brechtelandreas@gmail.com>
2021-06-03 15:57:20 +0200

    deck intake tool

--- server/tools/deckIntake.js
+++ server/tools/deckIntake.js
@@ -1,21 +1,47 @@
+/* Quick and dirty intake tool to get from images and card texts to the desired deck.json struckture
+ * HOWTO: Place images in subdir 'img'
+ * Place card text in csv with each card beeing in one row. Make sure to name first row: 'CardTexts'
+ * Create subdir 'imgConv'
+ * Run 'node .\deckIntake.js'
+ */
+
 const csv = require('csv-parser');
 const fs = require('fs');
+const path = require('path');
+
+const imgPath = __dirname + '/img/';
+const convImgPath = __dirname + '/imgConv/';
 
 const cardsArray = [];
+const memesArray = [];
 
 const emptyDeck = {
   memeCards: [],
   whiteCards: [],
 };
 
+let cardCounter = 0;
+
 fs.createReadStream('cardsRawData.csv')
   .pipe(csv())
   .on('data', (row) => {
-    cardsArray.push({ text: row.CardTexts, cardId: '' });
+    cardsArray.push({ text: row.CardTexts, cardId: `C${cardCounter++}` });
   })
   .on('end', () => {
-    console.log('CSV file successfully processed');
+    const fileDir = fs.readdirSync(imgPath);
+
+    fileDir.forEach((filename, i) => {
+      const newFileName = `meme${i}${path.extname(filename)}`;
+
+      fs.renameSync(imgPath + filename, convImgPath + newFileName);
+      memesArray.push({ name: newFileName, cardId: `M${i}` });
+    });
+
     emptyDeck.whiteCards = cardsArray;
-    let data = JSON.stringify(emptyDeck);
+    emptyDeck.memeCards = memesArray;
+
+    const data = JSON.stringify(emptyDeck);
+
+    console.log('CSV and IMGs successfully processed');
     fs.writeFileSync('deck.json', data);
   });
